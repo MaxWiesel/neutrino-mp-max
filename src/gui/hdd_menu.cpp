a209d62205e6d6b15481d997068154321cd5b378
max_10 <max_10@gmx.de>
2017-10-23 22:27:15 +0200

    hdd-menu: hide mmcblk devices from hdd-menu

--- src/gui/hdd_menu.cpp
+++ src/gui/hdd_menu.cpp
@@ -190,7 +190,11 @@ void CHDDMenuHandler::getBlkIds()
 	{
 		while ((mnt = getmntent(mountFile)) != NULL)
 		{
-			if (strncmp(mnt->mnt_fsname, "/dev/sd", 7) && strncmp(mnt->mnt_fsname, "/dev/hd", 7))
+			if (strncmp(mnt->mnt_fsname, "/dev/sd", 7) && strncmp(mnt->mnt_fsname, "/dev/hd", 7)
+#if HAVE_ARM_HARDWARE
+				&& (strncmp(mnt->mnt_fsname, "/dev/mmcblk", 11)
+#endif
+			)
 				continue;
 			hdd_s hdd;
 			hdd.devname = std::string(mnt->mnt_fsname +5 );
