17bf75e06846fa31e6998f804a15c52613a953db
max_10 <max_10@gmx.de>
2017-12-19 23:36:11 +0100

    infoviewer: fix colorsettings

--- src/gui/infoviewer.cpp
+++ src/gui/infoviewer.cpp
@@ -400,7 +400,7 @@ void CInfoViewer::paintBackground(int col_NumBox)
 		numbox->enableShadow(CC_SHADOW_ON, OFFSET_SHADOW, true);
 	}else
 		numbox->setDimensionsAll(BoxStartX, y_numbox, ChanWidth, ChanHeight);
-	numbox->setColorBody(g_settings.theme.infobar_gradient_top ? COL_MENUHEAD_PLUS_0 : col_NumBox);
+	numbox->setColorBody(g_settings.theme.infobar_gradient_top ? COL_INFOBAR_PLUS_3 : col_NumBox);
 	numbox->enableColBodyGradient(g_settings.theme.infobar_gradient_top, g_settings.theme.infobar_gradient_top ? COL_INFOBAR_PLUS_0 : col_NumBox, g_settings.theme.infobar_gradient_top_direction);
 	numbox->setCorner(c_rad_mid, CORNER_ALL);
 	numbox->paint(CC_SAVE_SCREEN_NO);
@@ -416,7 +416,7 @@ void CInfoViewer::paintHead()
 	}else
 		header->setDimensionsAll(head_x, ChanNameY, head_w, time_height);
 
-	header->setColorBody(g_settings.theme.infobar_gradient_top ? COL_MENUHEAD_PLUS_0 : COL_INFOBAR_PLUS_0);
+	header->setColorBody(g_settings.theme.infobar_gradient_top ? COL_INFOBAR_PLUS_3 : COL_INFOBAR_PLUS_0);
 	header->enableColBodyGradient(g_settings.theme.infobar_gradient_top, COL_INFOBAR_PLUS_0, g_settings.theme.infobar_gradient_top_direction);
 	clock->setColorBody(header->getColorBody());
 
@@ -457,7 +457,7 @@ void CInfoViewer::paintBody()
 	body->setCorner(RADIUS_LARGE, (zap_mode) ? CORNER_BOTTOM : CORNER_NONE);
 	body->enableShadow(zap_mode ? CC_SHADOW_ON : CC_SHADOW_RIGHT | CC_SHADOW_CORNER_TOP_RIGHT | CC_SHADOW_CORNER_BOTTOM_RIGHT);
 
-	body->setColorBody(g_settings.theme.infobar_gradient_body ? COL_MENUHEAD_PLUS_0 : COL_INFOBAR_PLUS_0);
+	body->setColorBody(g_settings.theme.infobar_gradient_body ? COL_INFOBAR_PLUS_3 : COL_INFOBAR_PLUS_0);
 	body->enableColBodyGradient(g_settings.theme.infobar_gradient_body, COL_INFOBAR_PLUS_0, g_settings.theme.infobar_gradient_body_direction);
 
 	body->paint(CC_SAVE_SCREEN_NO);
