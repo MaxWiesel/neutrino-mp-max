f37c6072e1a55f0984e6f387d700e52c0aee5f40
GetAway <get-away@t-online.de>
2018-11-27 00:59:06 +0100

    activate streaminfo2 for non COOL_HARDWARE

--- src/gui/user_menue.cpp
+++ src/gui/user_menue.cpp
@@ -49,7 +49,11 @@
 #include "subchannel_select.h"
 #include "favorites.h"
 #include "audio_select.h"
+#if HAVE_COOL_HARDWARE
+#include "streaminfo1.h"
+#else
 #include "streaminfo2.h"
+#endif
 #include "epgplus.h"
 #include "epgview.h"
 #include "eventlist.h"
