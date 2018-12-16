6e125db2899e1e4e2819a124fe8219cdab4646bb
vanhofen <vanhofen@gmx.de>
2018-12-16 01:17:01 +0100

    - menu: add possibility to get action state of menu widget

--- src/gui/widget/menue.h
+++ src/gui/widget/menue.h
@@ -667,6 +667,7 @@ class CMenuWidget : public CMenuTarget, public CComponentsSignals
 
 		void setNextShortcut(int sc) { nextShortcut = sc; };
 		int getNextShortcut() { return nextShortcut; };
+		bool gotAction() { return !no_action; };
 };
 
 class CPINProtection
