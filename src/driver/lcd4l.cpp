72e1ce3efcac17e6c5d86e8dd846a8c5b69954bd
GetAway <get-away@t-online.de>
2019-01-05 18:29:34 +0100

    l4l: fix LOGO_DUMMY; use system icon

--- src/driver/lcd4l.cpp
+++ src/driver/lcd4l.cpp
@@ -62,10 +62,9 @@ extern CPictureViewer *g_PicViewer;
 
 #define LCD_DATADIR		"/tmp/lcd/"
 
-#define LCD_ICONSDIR		LCD4L_ICONSDIR
 #define ICONSEXT		".png"
 
-#define LOGO_DUMMY		LCD_ICONSDIR "blank.png"
+#define LOGO_DUMMY		ICONSDIR "/blank.png"
 
 #define BRIGHTNESS		LCD_DATADIR "brightness"
 #define BRIGHTNESS_STANDBY	LCD_DATADIR "brightness_standby"
