8a3685a502583ccc193c14f0e352d0545b426130
vanhofen <vanhofen@gmx.de>
2019-01-24 22:05:37 +0100

    - pictureviewer: re-introduce pictureviewer.start/end scripts

--- src/gui/pictureviewer.cpp
+++ src/gui/pictureviewer.cpp
@@ -78,6 +78,8 @@
 #include <hardware/video.h>
 extern cVideo * videoDecoder;
 
+#define PICTUREVIEWER_START_SCRIPT CONFIGDIR "/pictureviewer.start"
+#define PICTUREVIEWER_END_SCRIPT CONFIGDIR "/pictureviewer.end"
 
 //------------------------------------------------------------------------
 bool comparePictureByDate (const CPicture& a, const CPicture& b)
@@ -188,6 +190,10 @@ int CPictureViewerGui::exec(CMenuTarget* parent, const std::string & actionKey)
 	if (parent)
 		parent->hide();
 
+	puts("[pictureviewer.cpp] executing " PICTUREVIEWER_START_SCRIPT ".");
+	if (my_system(PICTUREVIEWER_START_SCRIPT) != 0)
+		perror(PICTUREVIEWER_START_SCRIPT " failed");
+
 	// remember last mode
 	m_LastMode = CNeutrinoApp::getInstance()->getMode();
 	// tell neutrino we're in pic_mode
@@ -216,6 +222,10 @@ int CPictureViewerGui::exec(CMenuTarget* parent, const std::string & actionKey)
 		//g_Zapit->unlockPlayBack();
 		CZapit::getInstance()->EnablePlayback(true);
 	}
+
+	puts("[pictureviewer.cpp] executing " PICTUREVIEWER_END_SCRIPT ".");
+	if (my_system(PICTUREVIEWER_END_SCRIPT) != 0)
+		perror(PICTUREVIEWER_END_SCRIPT " failed");
 
 	// Restore previous background
 	if (usedBackground) {
