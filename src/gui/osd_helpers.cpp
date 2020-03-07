eb8c0f8c549ced770cc2d119f963c98c5acae8c5
Markus Volk <f_l_k@t-online.de>
2020-03-07 05:46:37 +0100

    osd_helpers.cpp: remove unneeded #ifdef conditions

--- src/gui/osd_helpers.cpp
+++ src/gui/osd_helpers.cpp
@@ -144,29 +144,19 @@ int COsdHelpers::isVideoSystem1080(int res)
 	    (res == VIDEO_STD_1080I50) ||
 	    (res == VIDEO_STD_1080P30) ||
 	    (res == VIDEO_STD_1080P24) ||
-	    (res == VIDEO_STD_1080P25))
-		return true;
-
-#ifdef BOXMODEL_CS_HD2
-	if ((res == VIDEO_STD_1080P50) ||
+	    (res == VIDEO_STD_1080P25) ||
+	    (res == VIDEO_STD_1080P50) ||
 	    (res == VIDEO_STD_1080P60) ||
 	    (res == VIDEO_STD_1080P2397) ||
-	    (res == VIDEO_STD_1080P2997))
-		return true;
-#endif
-
-#if HAVE_SH4_HARDWARE
-	if ((res == VIDEO_STD_1080P50))
-		return true;
-#endif
-
-#if HAVE_ARM_HARDWARE
-	if ((res == VIDEO_STD_1080P50) ||
-	    (res == VIDEO_STD_1080P60) ||
+	    (res == VIDEO_STD_1080P2997) ||
 	    (res == VIDEO_STD_2160P24) ||
 	    (res == VIDEO_STD_2160P25) ||
 	    (res == VIDEO_STD_2160P30) ||
 	    (res == VIDEO_STD_2160P50))
+		return true;
+
+#if HAVE_SH4_HARDWARE
+	if ((res == VIDEO_STD_1080P50))
 		return true;
 #endif
 
