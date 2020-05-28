4478d09f12ad3f1ea1717128e4dbb5aac311cc2d
Frankenstone <dampf_acc@yahoo.com>
2020-05-29 00:44:16 +0200

    add menu hint unicable configuration

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -1727,6 +1727,7 @@ const char * locale_real_names[] =
 	"menu.hint_tools",
 	"menu.hint_tvmode",
 	"menu.hint_tvradio_switch",
+	"menu.hint_uni_setup",
 	"menu.hint_upnp",
 	"menu.hint_vfd",
 	"menu.hint_vfd_brightness",
