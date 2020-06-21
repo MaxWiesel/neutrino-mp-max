f06fc124633a9d0a6a47baf19596dd894da07343
vanhofen <vanhofen@gmx.de>
2020-06-21 23:21:20 +0200

    - vfd_setup: execute glcd-setup directly if it's the only item

--- src/gui/vfd_setup.cpp
+++ src/gui/vfd_setup.cpp
@@ -131,6 +131,8 @@ int CVfdSetup::showSetup()
 	CMenuWidget *vfds = new CMenuWidget(LOCALE_MAINMENU_SETTINGS, NEUTRINO_ICON_LCD, width, MN_WIDGET_ID_VFDSETUP);
 	vfds->addIntroItems(LOCALE_LCDMENU_HEAD);
 
+	int initial_count = vfds->getItemsCount();
+
 	CMenuForwarder * mf;
 
 #ifndef HAVE_DUCKBOX_HARDWARE
@@ -229,6 +231,7 @@ int CVfdSetup::showSetup()
 	}
 #endif
 
+	CMenuItem* glcd_setup = NULL;
 #ifdef ENABLE_GRAPHLCD
 	GLCD_Menu glcdMenu;
 #ifdef ENABLE_LCD4LINUX
@@ -238,11 +241,18 @@ int CVfdSetup::showSetup()
 #endif
 	{
 		vfds->addItem(GenericMenuSeparatorLine);
-		vfds->addItem(new CMenuForwarder(LOCALE_GLCD_HEAD, true, NULL, &glcdMenu, NULL, CRCInput::RC_blue));
-	}
+		glcd_setup = new CMenuForwarder(LOCALE_GLCD_HEAD, true, NULL, &glcdMenu, NULL, CRCInput::RC_blue);
+		vfds->addItem(glcd_setup);	}
 #endif
 
-	int res = vfds->exec(NULL, "");
+	int res;
+	if (glcd_setup && (vfds->getItemsCount() == initial_count + 1))
+	{
+		// glcd-setup is the only item; execute directly
+		res = glcd_setup->exec(NULL);
+	}
+	else
+		res = vfds->exec(NULL, "");
 
 	delete vfds;
 	return res;
