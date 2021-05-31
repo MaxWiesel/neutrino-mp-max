e4dd7befc41e05058723bd1b45b56e4eb3a42de6
vanhofen <vanhofen@gmx.de>
2021-05-31 23:23:43 +0200

    - lcd4linux: fix first start

--- src/driver/lcd4l.cpp
+++ src/driver/lcd4l.cpp
@@ -614,7 +614,7 @@ void CLCD4l::ParseInfo(uint64_t parseID, bool newID, bool firstRun)
 
 	/* ----------------------------------------------------------------- */
 
-	if (newID || parseID == NeutrinoModes::mode_audio || parseID == NeutrinoModes::mode_ts)
+	if (firstRun || newID || parseID == NeutrinoModes::mode_audio || parseID == NeutrinoModes::mode_ts)
 	{
 		std::string Service = "";
 		int ChannelNr = 0;
@@ -809,6 +809,7 @@ void CLCD4l::ParseInfo(uint64_t parseID, bool newID, bool firstRun)
 				case 1:
 					Layout = DisplayType + "large";
 					break;
+				case 0:
 				default:
 					Layout = DisplayType + "standard";
 			}
