0b465602ae5ba798aa215062d5d4aedca33564a1
Jacek Jendrzej <overx300@gmail.com>
2021-09-19 13:18:08 +0200

    webtv: add prefered quality option for use in webtv plugins

--- src/system/locals_intern.h
+++ src/system/locals_intern.h
@@ -2959,6 +2959,7 @@ const char * locale_real_names[] =
 	"webtv.head",
 	"webtv.xml",
 	"webtv.xml.auto",
+	"webtv.xml.pref_quality",
 	"window_size",
 	"wizard.initial_settings",
 	"wizard.install_settings",
