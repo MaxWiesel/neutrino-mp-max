7609d9025f710d55c785ab0d148aa53d5bf43f6b
vanhofen <vanhofen@gmx.de>
2022-09-13 22:48:35 +0200

    - lcd4l: fix moviebrowser handling ...
    * use Movieplayer/Moviebrowser logos
    * remove RemoveEventFile()
    * rename NewParseID => new_ParseID

--- src/driver/lcd4l.h
+++ src/driver/lcd4l.h
@@ -64,7 +64,6 @@ class CLCD4l
 		int	RemoveFile(const char *file);
 
 		int	CreateEventFile(std::string content = "", bool convert = false);
-		int	RemoveEventFile();
 		int	CreateMenuFile(std::string content = "", bool convert = false);
 		int	RemoveMenuFile();
 
